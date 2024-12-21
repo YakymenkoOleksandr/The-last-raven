f0baef24485cf0eef90dd284bfa16e4d8e691fa6
YakymenkoOleksandr <93155369+YakymenkoOleksandr@users.noreply.github.com>
2024-12-21 03:09:58 +0200

    addcomit1
    Створено проект The last ravon за допомогою Vite, інстальовано проект, створені взаємозв"язки, встановлено пакети pixi та howler. Додані картинки, зроблені налаштувані Vite.

---
+++ src/common/textures.js
@@ -0,0 +1,22 @@
+export const allTextureKeys = {
+    shipBlue: 'shipBlue',
+    asteroid: 'asteroid',
+    bomb: 'bomb',
+    musicOn: 'musicOn',
+    musicOff: 'musicOff',
+    effectsOn: 'effectsOn',
+    effectsOff: 'effectsOff',
+    enemyShip: 'enemyShip',
+    spaseShip: 'spaseShip',
+    bullet1: 'bullet1',
+    bullet2: 'bullet2',
+    explosion1: 'explosion1',
+    explosion2: 'explosion2',
+    explosion3: 'explosion3',
+}
+
+const appTextures = {
+    [allTextureKeys.shipBlue]: '../../public/assets/sprites/enemys/shipBlue.png',
+    [allTextureKeys.asteroid]: '../../public/assets/sprites/enemys/asteroid.png',
+    
+}
