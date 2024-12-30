b2d7b3be040ac7c8bba9e69867627e66b5e7fe2b
YakymenkoOleksandr <93155369+YakymenkoOleksandr@users.noreply.github.com>
2024-12-31 01:38:12 +0200

    addcomit
    Пофікшено синтаксис з старого на новий.

--- src/common/assets.js
+++ src/common/assets.js
@@ -1,14 +1,20 @@
 import { Assets } from "pixi.js";
 import appTextures, { allTextureKeys } from "./textures";
 
+// Додати текстури в Assets з використанням нового API
 Object.entries(appTextures).forEach(([key, value]) => {
-  Assets.add(key, value);
+  Assets.add({
+    alias: key,  // ключ для доступу до текстури
+    src: value,  // шлях до текстури
+  });
 });
 
 const textures = new Map();
 
 export const loadAssets = (onProgress) => {
   const keys = Object.entries(allTextureKeys).map(([key, value]) => value);
+  
+  // Завантаження текстур
   Assets.load([...keys], onProgress).then((data) => {
     Object.entries(data).forEach(([key, value]) => {
       textures.set(key, value);
